815c630bee7477c2e8dc0e42610115b8067ffc23
Alex Stern <stern@cmu.edu>
2014-02-23 12:37:45 -0500

    Initial project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agora::Application.config.secret_token = 'a053dc299e7e4b933b6a7acd622257782453d30b2ccdf9d128efb72469da595929e04f3f2d24f4212fd77595ad7706d7543d5edff37fafdb9677e4621b1209cf'
