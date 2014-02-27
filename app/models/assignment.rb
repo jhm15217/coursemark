475f5f50f1bdd44bd4287b08f2b92827495483d0
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-02-27 11:02:18 -0500

    Fixed model type bugs. Gave assignments controller dispatch abilities.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,5 @@
 class Assignment < ActiveRecord::Base
-  attr_accessible :course_id, :draft, :review_due, :reviews_required, :submission_due
+  attr_accessible :course_id, :draft, :review_due, :reviews_required, :submission_due, :name
 
   # Relationships
   belongs_to :course
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140226044715) do
+ActiveRecord::Schema.define(:version => 20140227155836) do
 
   create_table "assignments", :force => true do |t|
     t.datetime "submission_due"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20140226044715) do
     t.integer  "course_id"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "name"
   end
 
   create_table "courses", :force => true do |t|
@@ -49,7 +50,7 @@ ActiveRecord::Schema.define(:version => 20140226044715) do
     t.boolean  "instructor"
     t.boolean  "active"
     t.integer  "user_id"
-    t.boolean  "course_id"
+    t.integer  "course_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
