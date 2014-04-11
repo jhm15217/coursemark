75946be98bb023c762e1ce66a8905a4bac69b183
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-04-11 16:48:18 -0400

    Flattened migrations

--- db/migrate/20140228013042_change_data_type_for_submission.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataTypeForSubmission < ActiveRecord::Migration
-  def up
-  	change_column :submissions, :submission, :string
-  end
-
-  def down
-  	change_column :submissions, :submission, :binary
-  end
-end
