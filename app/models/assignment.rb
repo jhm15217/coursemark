6934a8814f1e885b59e1a7922c4bfc596a44db84
Demerson Hayden <ddemerso@andrew.cmu.edu>
2014-04-16 22:18:36 -0400

    More robust validation for deadlines

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -14,11 +14,13 @@ class Assignment < ActiveRecord::Base
   # Validations
   validates_inclusion_of :draft, :in => [true, false], :message => "must be true or false"
   validates_numericality_of :reviews_required, :only_integer => true, :greater_than_or_equal_to  => 0
-  validates_date :submission_due, :allow_nil => true
-  validates_date :review_due, :allow_nil => true
+  validates_datetime :submission_due, :allow_nil => true, :before => :review_due, :before_message => "Submission deadline must be before review deadline"
+  validates_datetime :review_due, :allow_nil => true, :after => :submission_due, :after_message => "Review deadline must be after submission deadline"
+  
   # submission and review due dates can only be changed if they haven't passed
-  validates :submission_due, :deadline => true, :on => :update, :unless => :draft?, :if => :submission_due_changed?
-  validates :review_due, :deadline => true, :on => :update, :unless => :draft?, :if => :review_due_changed?
+  validate :submission_deadline_not_passed
+  validate :review_deadline_not_passed
+
   # make sure the number of reviews required is feasible given class size
   validate :reviews_required_feasible
   # only allow changes to reviews_required if we are still taking submissions
@@ -42,6 +44,20 @@ class Assignment < ActiveRecord::Base
   end
 
   private
+  def submission_deadline_not_passed
+    puts self.submission_due
+    puts self.submission_due_was
+    if self.submission_due < Time.now and self.submission_due.to_i != self.submission_due_was.to_i
+      errors.add(:submission_due, "Can't change submission deadline after it has passed")
+    end
+  end
+
+  def review_deadline_not_passed
+    if self.review_due < Time.now and self.review_due.to_i != self.review_due_was.to_i
+      errors.add(:review_due, "Can't change review deadline after it has passed")
+    end
+  end
+  
   def update_evaluations
     self.submissions.each {|submission| submission.save!}
   end
@@ -53,7 +69,7 @@ class Assignment < ActiveRecord::Base
   end
 
   def submissions_open
-    if submission_due < Date.today
+    if submission_due < Time.now
       errors.add(:reviews_required, "Can't change number of reviews required after submission deadline has passed.")
     end
   end
