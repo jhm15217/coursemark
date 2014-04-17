0bfccb6bc031394cca623fc282263ac8533b980f
Demerson Hayden <ddemerso@andrew.cmu.edu>
2014-04-16 22:18:36 -0400

    More robust validation for deadlines

--- app/validators/deadline_validator.rb
+++
@@ -1,9 +0,0 @@
-class DeadlineValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless record[attribute].nil?
-    	if record[attribute] > Date.today
-      		record.errors.add(attribute, "Can't change deadline that's already past")
-    	end
-    end
-  end
-end
