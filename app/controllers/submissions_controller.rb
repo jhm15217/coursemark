d81138bb19671666f992913d90061a5116bc0849
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-04-21 00:25:21 -0400

    Fixed submission redirect bug

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -53,7 +53,7 @@ class SubmissionsController < ApplicationController
         format.html { redirect_to [@course, @assignment] }
         format.json { render json: @submission, status: :created, location: @submission }
       else
-        format.html { render action: "new" }
+        format.html { redirect_to [@course, @assignment] }
         format.json { render json: @submission.errors, status: :unprocessable_entity }
       end
     end
