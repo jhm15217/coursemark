11b5838e9c3560788f6078b856432f5a937a1023
Alex Stern <alex@alexstern.com>
2014-04-23 20:02:27 -0400

    Auto-save bug fix

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -29,22 +29,25 @@ function submitForms() {
 (function(f){function l(g,h){function d(a){if(!e){e=true;c.start&&c.start(a,b)}}function i(a,j){if(e){clearTimeout(k);k=setTimeout(function(){e=false;c.stop&&c.stop(a,b)},j>=0?j:c.delay)}}var c=f.extend({start:null,stop:null,delay:400},h),b=f(g),e=false,k;b.keypress(d);b.keydown(function(a){if(a.keyCode===8||a.keyCode===46)d(a)});b.keyup(i);b.blur(function(a){i(a,0)})}f.fn.typing=function(g){return this.each(function(h,d){l(d,g)})}})(jQuery);
 
   for (var i=0; i<forms.length; i++) {
-    textarea = $(forms[i]).find('textarea[name="response[peer_review]"]')[0];
+    var textarea = $(forms[i]).find('textarea[name="response[peer_review]"]')[0];
 
     // Checkbox change
-    checkbox = $($(forms[i])[0]).find('input[name="response[scale_id]"]').change(function(){
-      $(textarea).parent().parent().parent().find('.savedStatus').html('saving...');
-      $($(textarea).parent().parent()[0]).trigger('submit.rails');
+    var checkbox = $(forms[i]).find('input[name="response[scale_id]"]').parent().parent();
+    checkbox.change(function(){
+      console.log('changed radio button');
+      $(this).parent().parent().find('.savedStatus').html('saving...');
+      $($(this).parent()[0]).trigger('submit.rails');
     });
 
     // Textarea change
       $(textarea).typing({
         start: function (event, $elem) {
-            $($(textarea).parent().parent().parent().find('.savedStatus')[0]).html('typing...');
+            console.log($elem);
+            $($elem.parent().parent().parent().find('.savedStatus')[0]).html('typing...');
         },
         stop: function (event, $elem) {
-            $($(textarea).parent().parent().parent().find('.savedStatus')[0]).html('saving...');
-            $($(textarea).parent().parent()[0]).trigger('submit.rails');
+            $($elem.parent().parent().parent().find('.savedStatus')[0]).html('saving...');
+            $($elem.parent().parent()[0]).trigger('submit.rails');
         },
         delay: 1000
       });
