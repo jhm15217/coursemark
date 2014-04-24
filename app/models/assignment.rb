3b69a4af71255b0f10375ea94df2c36fe8939b89
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-04-23 20:16:02 -0400

    Debug

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -92,6 +92,7 @@ class Assignment < ActiveRecord::Base
   def make_dates
     @offset = Time.zone.now.to_s.split(' ')[2]
     self.submission_due = DateTime.parse("#{@submission_due_date} #{@submission_due_time + @offset}")
+    puts self.submission_due
     self.review_due = DateTime.parse("#{@review_due_date} #{@review_due_time + @offset}")
   end
 
