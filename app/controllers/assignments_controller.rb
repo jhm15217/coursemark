5d441b3903263e67731092c483db66554c5a2185
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-05-22 00:10:21 -0700

    Redirect to course roster if there are no assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -28,7 +28,8 @@ class AssignmentsController < ApplicationController
       end
     else
       if current_user.instructor?(@course)
-        @URL = { :action => 'new' }
+        #@URL = { :action => 'new' }
+        @URL = registrations_path(:course => @course.id)
       else
         @URL = edit_user_path(current_user, :course => @course.id)
       end
