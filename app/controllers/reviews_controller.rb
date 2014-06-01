8d4757370e07099060c6ef2ae044cf294e7b46f3
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-06-01 00:02:40 -0400

    Initial commit

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  before_filter :get_assignment, :get_course
+
+  def index
+  	authorize! :manage, :reviews
+  	
+  	respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def get_assignment
+    if params[:assignment_id]
+      @assignment = Assignment.find(params[:assignment_id])
+    end
+  end
+
+  def get_course
+    if params[:course_id]
+      @course = Course.find(params[:course_id])
+    end
+  end
+end
