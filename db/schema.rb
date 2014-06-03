85023a1189edbe535599fbf70fa33b289ec96bf0
Kevin Schaefer <kevinjschaefer@gmail.com>
2014-06-02 23:17:49 -0400

    Revert "Revert "Rolled back unnecessary manual assignment option""
    This reverts commit fd5e2c9f08593181250bb3769f9aff65ebd92380.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140531230337) do
+ActiveRecord::Schema.define(:version => 20140603031401) do
 
   create_table "assignments", :force => true do |t|
     t.datetime "submission_due"
@@ -19,10 +19,9 @@ ActiveRecord::Schema.define(:version => 20140531230337) do
     t.integer  "reviews_required"
     t.boolean  "draft"
     t.integer  "course_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
     t.string   "name"
-    t.boolean  "manual_assignment"
   end
 
   create_table "courses", :force => true do |t|
