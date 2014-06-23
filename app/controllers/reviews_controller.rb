45a3b3ca4784a25c98c1fb3b50e46d046a3d18f9
James Morris <james.morris@cmu.edu>
2014-06-22 22:07:58 -0400

    review display handles teams

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,7 +4,7 @@ class ReviewsController < ApplicationController
   def index
   	authorize! :manage, :reviews
   	
-    @students = @assignment.course.users.where("instructor = 'f'")
+    @students = @assignment.get_students
 
   	respond_to do |format|
       format.html # index.html.erb
