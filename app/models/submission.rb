daa6dedd31e5ac1f83dd85c15af6dcbbd6e8286b
James Morris <james.morris@cmu.edu>
2014-06-25 08:39:25 -0400

    Pre-publication checks

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -75,12 +75,6 @@ class Submission < ActiveRecord::Base
   end
 
   def create_and_save_evaluations
-  	# if reviews_required has since become infeasible
-  	while ((self.assignment.course.get_real_students.length - 1) < self.assignment.reviews_required)
-  		self.assignment.reviews_required = self.assignment.reviews_required - 1
-  		self.assignment.save!
-  	end
-
   	# only run if the number of evaluations isn't the number required
   	if self.evaluations.length != self.assignment.reviews_required
 	  	self.evaluations.delete_all
