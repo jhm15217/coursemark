980161cb92184ca44621ad2222204220a9f88c12
James Morris <james.morris@cmu.edu>
2014-08-28 21:01:43 -0400

    Fixed radio button error and restored cosmetic changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -89,7 +89,7 @@ class ApplicationController < ActionController::Base
   end
 
   def login_student
-    $PUPPET = User.find(params[:student])
+#    $PUPPET = User.find(params[:student])
     redirect_to root_url
   end
 
@@ -102,7 +102,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user = $PUPPET || current_user_session && current_user_session.record
+    @current_user = current_user_session && current_user_session.record
     @current_user
   end
 
