fdc6e20c4f342cfd7824bc9da7f2c00be795ebc6
James Morris <james.morris@cmu.edu>
2014-09-13 12:26:20 -0400

    Clean up response boxes

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -38,8 +38,7 @@ function adjust_focus(elem) {
 (function(f){function l(g,h){function d(a){if(!e){e=true;c.start&&c.start(a,b)}}function i(a,j){if(e){clearTimeout(k);k=setTimeout(function(){e=false;c.stop&&c.stop(a,b)},j>=0?j:c.delay)}}var c=f.extend({start:null,stop:null,delay:400},h),b=f(g),e=false,k;b.keypress(d);b.keydown(function(a){if(a.keyCode===8||a.keyCode===46)d(a)});b.keyup(i);b.blur(function(a){i(a,0)})}f.fn.typing=function(g){return this.each(function(h,d){l(d,g)})}})(jQuery);
 
 for (var i=0; i<forms.length; i++) {
-    var textarea = $(forms[i]).find('textarea[name="response[peer_review]"]')[0];
-    var textarea2 = $(forms[i]).find('textarea[name="response[instructor_response]"]')[0];
+    var textarea = $(forms[i]).find('textarea[class="submissionTextArea fl"]')[0];
 
     // Checkbox change
     var checkbox = $(forms[i]).find('input[name="response[scale_id]"]').parent().parent();
@@ -60,21 +59,6 @@ for (var i=0; i<forms.length; i++) {
 
     // Textarea change
     $(textarea).typing({
-        start: function (event, $elem) {
-            $($elem.parent().parent().parent().find('.savedStatus')[0]).html('typing...');
-        },
-        stop: function (event, $elem) {
-            var tb = $($elem)
-            tb.height(tb.prop('scrollHeight'));
-
-            $($elem.parent().parent().parent().find('.savedStatus')[0]).html('saving...');
-            $($elem.parent().parent()[0]).trigger('submit.rails');
-        },
-        delay: 1000
-    });
-
-    // Textareas change
-    $(textarea2).typing({
         start: function (event, $elem) {
             $($elem.parent().parent().find('.savedStatus')[0]).html('typing...');
         },
