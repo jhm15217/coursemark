dd928c98959c7a6862f55628367f46b00c0cd8bc
James Morris <james.morris@cmu.edu>
2014-10-01 07:02:14 -0400

    print caches

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -22,7 +22,7 @@ class SubmissionsController < ApplicationController
     current_hash = Zlib.crc32(sort_direction + current_user.email +
                                   sortable.map{|record| record[:registration].id.to_s + record[:sort_key]}.reduce(:+))
     puts "Current: " + current_hash.to_s + ' Stored: ' + @assignment.sort_hash.to_s
-    pust 'Sort: ' +   @assignment.cached_sort.inspect
+    puts 'Sort: ' +   @assignment.cached_sort.inspect
     if current_hash == @assignment.sort_hash
       @students = @assignment.cached_sort.map{|r_id| Registration.find(r_id).user }
     else
