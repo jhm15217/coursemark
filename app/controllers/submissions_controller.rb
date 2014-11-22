4ef979c9095bc553a176fa64e310dc79b26ae66f
James Morris <james.morris@cmu.edu>
2014-11-21 21:41:29 -0500

    fix submissions index bug

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -14,7 +14,7 @@ class SubmissionsController < ApplicationController
     end
 
     registrations =  @course.registrations
-    @students = registrations.select{|r| r.user.pseudo ? @assignment.memberships.any?{|m| m.pseudo_user_id == r.user_id } : true }
+    @students = registrations.select{|r| r.user.pseudo ? @assignment.memberships.any?{|m| m.pseudo_user_id == r.user_id } : true }.map{|r| r.user }
     #.sort_by{|r| (r.section || "\177") + r.last_name + ' ' + r.first_name }.map{|r| r.user }
 
     # # Avoid sort if nothing has changed
