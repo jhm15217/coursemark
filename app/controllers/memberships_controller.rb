9feb604e4277233af5ecbe1e55f3404f7e1f8364
jhm <james.morris@cmu.edu>
2015-09-07 08:26:56 -0400

    delete .env

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -52,7 +52,10 @@ class MembershipsController < ApplicationController
     @assignment = Assignment.find(params[:assignment_id])
     @assignment.memberships.each {|membership| membership.destroy }    # Every upload is a complete team designations
 
-    params[:response][:teams].split("\r\n").each{ |line| add_teammate(line.split(',').map{|s| clean_csv_item(s)}) }
+    tm = params[:response][:teams].split("\r\n" )
+    tm.each{ |line|
+      add_teammate(line.split(',').map{|s| clean_csv_item(s)})
+    }
     @course.get_real_students.each do |student|
       if !@assignment.memberships.any?{|membership| membership.user_id == student.id }
         multi_flash(:notice, 'Student(s) without a team: ', student.email)
